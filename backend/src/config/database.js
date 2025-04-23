432ede9d20353c53781456a1822ab84e7b7e6b31
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-23 10:55:38 -0300

    add: backend

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/todo')
 
-MongoClient.connect("mongodb://localhost:27017/todo", {
+mongoose.connect('mongodb://localhost:27017/todo', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  });
+})
+
+module.exports = mongoose
